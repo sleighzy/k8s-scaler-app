a2b0b006c22232ca8188217aadf583a0ac1e06d1
Simon Leigh <simonl@conqahq.com>
2021-12-29 22:21:07 +1300

    Add API call to scale replicas and refetch updated deployment data
    The SWR mutate() function is used as a (hacky) means of invalidating the cache to
    cause the deployment list to be fetched again and all deployment items refreshed.

--- src/Deployment.tsx
+++ src/Deployment.tsx
@@ -1,18 +1,57 @@
+import useSWR, { useSWRConfig } from 'swr';
 import { DeploymentData } from './types';
 
 interface DeploymentProps {
   deployment: DeploymentData;
 }
 
+const apiServer = 'http://localhost';
+
 const Deployment = ({ deployment }: DeploymentProps): JSX.Element => {
+  const { mutate } = useSWRConfig();
+
   const scale = async (
     event: React.MouseEvent<HTMLAnchorElement>,
     deployment: DeploymentData,
     scale: number
   ) => {
+    const { namespace, name } = deployment.metadata;
+
     event.preventDefault();
     const replicas = deployment.spec.replicas + scale;
     console.log(`Scaling to ${replicas}`);
+
+    const payload = {
+      spec: {
+        replicas,
+      },
+    };
+
+    const response = await fetch(
+      `${apiServer}/apis/apps/v1/namespaces/${namespace}/deployments/${name}/scale`,
+      {
+        method: 'PATCH',
+        body: JSON.stringify({
+          spec: {
+            replicas,
+          },
+        }),
+        headers: { 'Content-Type': 'application/strategic-merge-patch+json' },
+      }
+    );
+
+    if (!response.ok) {
+      throw new Error(
+        `Failed to scale deployment ${name}/${namespace}, status: ${response.status}`
+      );
+    }
+
+    const data = await response.json();
+    console.log(data);
+
+    // trigger a re-validation so that the deployment list is fetched again and
+    // the new replicas count is displayed as each item in the list is rendered
+    mutate(`${apiServer}/apis/apps/v1/deployments`);
   };
 
   return (
@@ -34,7 +73,7 @@ const Deployment = ({ deployment }: DeploymentProps): JSX.Element => {
       <td className="px-6 py-4 whitespace-nowrap text-right text-sm font-medium">
         <a
           href="#"
-          className="text-indigo-600 hover:text-indigo-900"
+          className="text-blue-600 hover:text-indigo-900"
           onClick={(event) => scale(event, deployment, 1)}
         >
           Up
@@ -42,7 +81,7 @@ const Deployment = ({ deployment }: DeploymentProps): JSX.Element => {
         {' | '}
         <a
           href="#"
-          className="text-indigo-600 hover:text-indigo-900"
+          className="text-blue-600 hover:text-indigo-900"
           onClick={(event) => scale(event, deployment, -1)}
         >
           Down
