604da07da3e0595382683f4bb8665911bc39cb60
Simon Leigh <simonl@conqahq.com>
2021-12-29 23:35:51 +1300

    Add loading spinner while retrieving deployments

--- src/DeploymentList.tsx
+++ src/DeploymentList.tsx
@@ -26,7 +26,19 @@ const DeploymentList = (): JSX.Element => {
 
   if (error) return <div>Failed to retrieve deployment information</div>;
 
-  if (!data) return <div>Retrieving deployment information...</div>;
+  if (!data) {
+    return (
+      <div className="fixed top-0 left-0 right-0 bottom-0 w-full h-screen z-50 overflow-hidden bg-gray-700 opacity-75 flex flex-col items-center justify-center">
+        <div className="loader ease-linear rounded-full border-4 border-t-4 border-gray-200 h-12 w-12 mb-4"></div>
+        <h2 className="text-center text-white text-xl font-semibold">
+          Loading...
+        </h2>
+        <p className="w-1/3 text-center text-white">
+          Retrieving Kubernetes deployments
+        </p>
+      </div>
+    );
+  }
 
   return (
     <div>
