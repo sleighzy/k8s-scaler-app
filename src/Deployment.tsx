17b575370e5f1f1702e27bb241f2057e388a8696
Simon Leigh <simonl@conqahq.com>
2021-12-29 19:39:40 +1300

    Add event handler for scaling deployment replicas up and down

--- src/Deployment.tsx
+++ src/Deployment.tsx
@@ -4,32 +4,52 @@ interface DeploymentProps {
   deployment: DeploymentData;
 }
 
-const Deployment = ({ deployment }: DeploymentProps): JSX.Element => (
-  <tr key={`${deployment.metadata.namespace}|${deployment.metadata.name}`}>
-    <td className="px-6 py-4 whitespace-nowrap">
-      <div className="text-sm text-gray-900">{deployment.metadata.name}</div>
-      <div className="text-sm text-gray-500">
-        {deployment.metadata.namespace}
-      </div>
-    </td>
-    <td className="px-6 py-4 whitespace-nowrap">
-      <span className="px-2 inline-flex text-xs leading-5 font-semibold rounded-full bg-green-100 text-green-800">
-        Available
-      </span>
-    </td>
-    <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
-      {deployment.status.replicas} / {deployment.spec.replicas}
-    </td>
-    <td className="px-6 py-4 whitespace-nowrap text-right text-sm font-medium">
-      <a href="#" className="text-indigo-600 hover:text-indigo-900">
-        Up
-      </a>
-      {' | '}
-      <a href="#" className="text-indigo-600 hover:text-indigo-900">
-        Down
-      </a>
-    </td>
-  </tr>
-);
+const Deployment = ({ deployment }: DeploymentProps): JSX.Element => {
+  const scale = async (
+    event: React.MouseEvent<HTMLAnchorElement>,
+    deployment: DeploymentData,
+    scale: number
+  ) => {
+    event.preventDefault();
+    const replicas = deployment.spec.replicas + scale;
+    console.log(`Scaling to ${replicas}`);
+  };
+
+  return (
+    <tr key={`${deployment.metadata.namespace}|${deployment.metadata.name}`}>
+      <td className="px-6 py-4 whitespace-nowrap">
+        <div className="text-sm text-gray-900">{deployment.metadata.name}</div>
+        <div className="text-sm text-gray-500">
+          {deployment.metadata.namespace}
+        </div>
+      </td>
+      <td className="px-6 py-4 whitespace-nowrap">
+        <span className="px-2 inline-flex text-xs leading-5 font-semibold rounded-full bg-green-100 text-green-800">
+          Available
+        </span>
+      </td>
+      <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
+        {deployment.status.replicas} / {deployment.spec.replicas}
+      </td>
+      <td className="px-6 py-4 whitespace-nowrap text-right text-sm font-medium">
+        <a
+          href="#"
+          className="text-indigo-600 hover:text-indigo-900"
+          onClick={(event) => scale(event, deployment, 1)}
+        >
+          Up
+        </a>
+        {' | '}
+        <a
+          href="#"
+          className="text-indigo-600 hover:text-indigo-900"
+          onClick={(event) => scale(event, deployment, -1)}
+        >
+          Down
+        </a>
+      </td>
+    </tr>
+  );
+};
 
 export default Deployment;
